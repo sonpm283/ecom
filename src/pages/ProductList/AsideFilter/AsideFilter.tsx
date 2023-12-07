58150849c1dea3af7b390c7b93c366a809c2d6fc
PhanMinhSon <sonpm@hivelab.com>
2023-12-07 22:09:52 +0700

    Filter by rating star, remove filter

--- src/pages/ProductList/AsideFilter/AsideFilter.tsx
+++ src/pages/ProductList/AsideFilter/AsideFilter.tsx
@@ -10,6 +10,8 @@ import { schema, Schema } from 'src/utils/rules'
 import { yupResolver } from '@hookform/resolvers/yup'
 import { ObjectSchema } from 'yup'
 import { NoUndefinedField } from 'src/types/utils.type'
+import RatingStars from '../RatingStars'
+import { omit } from 'lodash'
 
 interface Props {
   categoriesData: Category[]
@@ -30,6 +32,7 @@ export default function AsideFilter({ categoriesData, queryConfig }: Props) {
     control,
     handleSubmit,
     trigger,
+    reset,
     formState: { errors }
   } = useForm<FormData>({
     defaultValues: {
@@ -53,6 +56,16 @@ export default function AsideFilter({ categoriesData, queryConfig }: Props) {
       }).toString()
     })
   })
+
+  const handleRemoveAll = () => {
+    console.log(omit(queryConfig, ['price_min', 'price_max', 'rating_filter', 'category']))
+    reset()
+    navigate({
+      pathname: path.home,
+      // tương đương ...queryConfig(path hiện tại) nhưng xoá đi price_min, price_max, rating_filter, category
+      search: createSearchParams(omit(queryConfig, ['price_min', 'price_max', 'rating_filter', 'category'])).toString()
+    })
+  }
 
   return (
     <div className='py-4'>
@@ -183,78 +196,17 @@ export default function AsideFilter({ categoriesData, queryConfig }: Props) {
       </div>
       <div className='bg-gray-300 h-[1px] my-4'></div>
       <div className='text-sm'>Đánh giá</div>
-      <ul className='my-3'>
-        <li className='py-1 pl-2'>
-          <Link to='' className='flex items-center text-sm'>
-            {Array(5)
-              .fill(0)
-              .map((_, index) => (
-                <svg viewBox='0 0 9.5 8' className='w-4 h- mr-1' key={index}>
-                  <defs>
-                    <linearGradient id='ratingStarGradient' x1='50%' x2='50%' y1='0%' y2='100%'>
-                      <stop offset={0} stopColor='#ffca11' />
-                      <stop offset={1} stopColor='#ffad27' />
-                    </linearGradient>
-                    <polygon
-                      id='ratingStar'
-                      points='14.910357 6.35294118 12.4209136 7.66171903 12.896355 4.88968305 10.8823529 2.92651626 13.6656353 2.52208166 14.910357 0 16.1550787 2.52208166 18.9383611 2.92651626 16.924359 4.88968305 17.3998004 7.66171903'
-                    />
-                  </defs>
-                  <g fill='url(#ratingStarGradient)' fillRule='evenodd' stroke='none' strokeWidth={1}>
-                    <g transform='translate(-876 -1270)'>
-                      <g transform='translate(155 992)'>
-                        <g transform='translate(600 29)'>
-                          <g transform='translate(10 239)'>
-                            <g transform='translate(101 10)'>
-                              <use stroke='#ffa727' strokeWidth='.5' xlinkHref='#ratingStar' />
-                            </g>
-                          </g>
-                        </g>
-                      </g>
-                    </g>
-                  </g>
-                </svg>
-              ))}
-            <span>trở lên</span>
-          </Link>
-        </li>
-        <li className='py-1 pl-2'>
-          <Link to='' className='flex items-center text-sm'>
-            {Array(5)
-              .fill(0)
-              .map((_, index) => (
-                <svg viewBox='0 0 9.5 8' className='w-4 h- mr-1' key={index}>
-                  <defs>
-                    <linearGradient id='ratingStarGradient' x1='50%' x2='50%' y1='0%' y2='100%'>
-                      <stop offset={0} stopColor='#ffca11' />
-                      <stop offset={1} stopColor='#ffad27' />
-                    </linearGradient>
-                    <polygon
-                      id='ratingStar'
-                      points='14.910357 6.35294118 12.4209136 7.66171903 12.896355 4.88968305 10.8823529 2.92651626 13.6656353 2.52208166 14.910357 0 16.1550787 2.52208166 18.9383611 2.92651626 16.924359 4.88968305 17.3998004 7.66171903'
-                    />
-                  </defs>
-                  <g fill='url(#ratingStarGradient)' fillRule='evenodd' stroke='none' strokeWidth={1}>
-                    <g transform='translate(-876 -1270)'>
-                      <g transform='translate(155 992)'>
-                        <g transform='translate(600 29)'>
-                          <g transform='translate(10 239)'>
-                            <g transform='translate(101 10)'>
-                              <use stroke='#ffa727' strokeWidth='.5' xlinkHref='#ratingStar' />
-                            </g>
-                          </g>
-                        </g>
-                      </g>
-                    </g>
-                  </g>
-                </svg>
-              ))}
-            <span>trở lên</span>
-          </Link>
-        </li>
-      </ul>
+
+      {/* RatingStar */}
+
+      <RatingStars queryConfig={queryConfig} />
       <div className='bg-gray-300 h-[1px] my-4'></div>
-      <Button className='w-full p-2 bg-orange text-white text-sm hover:bg-orange/80 uppercase'>Xoá tất cả</Button>
+      <Button
+        className='w-full p-2 bg-orange text-white text-sm hover:bg-orange/80 uppercase'
+        onClick={handleRemoveAll}
+      >
+        Xoá tất cả
+      </Button>
     </div>
   )
 }
